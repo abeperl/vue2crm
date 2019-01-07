51a154308438a881abb8311f1bbcff6f378a3d0c
Abe Perl <abeperl@gmail.com>
2019-01-06 20:59:38 -0500

    Make row clickable

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-  <v-data-table class="elevation-1" v-bind:headers="headers" v-bind:items="items" v-bind:search="search" v-bind:pagination="pagination" hide-actions>
+  <v-data-table 
+    class="elevation-1" 
+    @clickrow="clickRow(props.item)"
+    @click="clickRow(props.item)"
+    v-bind:headers="headers" 
+    v-bind:items="items" 
+    v-bind:search="search" 
+    v-bind:pagination="pagination" 
+    hide-actions>
       <template slot="headers" slot-scope="props">
           <tr>
             <th v-for="(header, index) in props.headers" :key="header.text"
@@ -11,9 +19,12 @@
             </th>
           </tr>
     </template>
-    <template class="body-2" slot="items" slot-scope="props">
-      <td  v-for="(header, index) in headers" :key="index" 
-          :class="[ index === 0? 'text-xs-left': 'text-xs-center', 'body-2']" v-if="header.value!==''">
+    <template class="body-2" slot="items" slot-scope="props" 
+      color="success">
+      <td v-for="(header, index) in headers" :key="index" 
+          :class="[ index === 0? 'text-xs-left': 'text-xs-center', 'body-2']" 
+          @click="clickRow(props.item)"
+          v-if="header.value !== ''">
         {{renderData(props.item, header)}}
       </td>
         <td class="text-xs-right">
@@ -61,7 +72,10 @@ export default {
         val = val ? "Yes" : "No";
       }
       return val;
-    }
+    },
+    clickRow (item) {
+      this.$emit("edit", item);
+    },
   },
   computed: {
     isNotEmpty () {
